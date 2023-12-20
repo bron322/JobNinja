5b8c721f29b9408db0c876796790ad967af91990
bron322 <lebron13145@gmail.com>
2023-12-20 18:25:18 +0800

    added rapid_api_key for concealing purposes

--- JobNinja/hook/useFetch.js
+++ JobNinja/hook/useFetch.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { RAPID_API_KEY } from "@env";
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
@@ -10,7 +11,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "6f0b656b7amshef88254fb1d0c34p117cc1jsned76168d814f",
+      "X-RapidAPI-Key": RAPID_API_KEY,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
